c4765ab26a4fb66970f971fdca577c6cd7f9a8a0
Tess <tniehoff24@gmail.com>
2020-11-15 20:44:36 -0500

    Updating code from Code SandBox and importing Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,16 +1,99 @@
 import React from "react";
-import Weather from "./Weather";
-import './App.css';
+import "./App.css";
+//import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Hello World!</h1>
-       <Weather city="Tokyo"/>
-      </header>
+      <div className="around">
+        <div className="container">
+          <div className="row">
+            <div md="auto" className="weatherstats">
+              <h1>My weather in:</h1>
+              <h2 id="current-city">Baltimore</h2>
+            </div>
+            <div md="auto" className="weatherstats">
+              <h2 id="today-temp">22°</h2>
+              <h3>
+                {/*<img src="" alt="Clear" className="iconSet" id="icon-main" />*/}
+                ⛅ C / F
+              </h3>
+            </div>
+            <div md="auto" className="weatherstats2">
+              <h4 id="date">30 August</h4>
+              <h4 id="humidity">Humidity: 80%</h4>
+              <h4 id="wind">Wind: 3km/hr</h4>
+              <h4 id="condition">Clear</h4>
+            </div>
+          </div>
+
+          <div className="row" id="forecast">
+            <div className="card">
+              <h5>
+                <strong>23°</strong>
+                /18°
+              </h5>
+              🌨️
+              <p>Tuesday</p>
+            </div>
+            <div className="card">
+              <h5>
+                <strong>23°</strong>
+                /18°
+              </h5>
+              🌨️
+              <p>Tuesday</p>
+            </div>
+            <div className="card">
+              <h5>
+                <strong>23°</strong>
+                /18°
+              </h5>
+              🌨️
+              <p>Tuesday</p>
+            </div>
+            <div className="card">
+              <h5>
+                <strong>23°</strong>
+                /18°
+              </h5>
+              🌨️
+              <p>Tuesday</p>
+            </div>
+            <div className="card">
+              <h5>
+                <strong>23°</strong>
+                /18°
+              </h5>
+              🌨️
+              <p>Tuesday</p>
+            </div>
+          </div>
+        </div>
+
+        <form id="form-input-city">
+          <input
+            type="text"
+            id="text-input-city"
+            placeholder="It's always sunny in..."
+          />
+          <button type="submit" className="searchButton">
+            Search
+          </button>
+          <button type="submit" className="refreshButton" id="refresh-button">
+            Refresh
+          </button>
+        </form>
+        <div className="row">
+          <small>
+            This application is an open-sourced project by{" "}
+            <a href="https://github.com/tniehoff24/SheCodes-Weather-App">
+              Tess Niehoff
+            </a>
+            .
+          </small>
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
--- src/Weather.js
+++ src/Weather.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 export default function Weather(props){
 
     function handleResponse(response){
-        alert(`The weather in ${props.city} is ${response.data.main.temp} degree C`)
+        alert(`The weather in New York is ${response.data.main.temp} degree C`)
     }
     let apiKey = "47198918c704fa3509c18d918eacec0c";
     let apiURL =`https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
